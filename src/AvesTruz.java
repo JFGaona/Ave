95954c278eeaca616527a0c58ebd27c762e046de
JFGaona <gaonajonathan5@gmail.com>
2024-02-12 18:45:50 -0500

    Se agregó "no vuela" a AvesTruz

--- src/AvesTruz.java
+++ src/AvesTruz.java
@@ -4,4 +4,9 @@ public class AvesTruz extends Ave{
     public void makeSound(){
         System.out.println("<<Silencio>>");
     }
+
+    @Override
+    public void vuela() {
+        System.out.println("no vuela");
+    }
 }
