131b900aa8808bd8de37430ce8edf01da00d6a16
JFGaona <gaonajonathan5@gmail.com>
2024-02-12 19:35:25 -0500

    JUNIT agregado correctamente al MAVEN y actualizado dentro del proyecto.

--- src/Test.java
+++ src/Test.java
@@ -1,16 +1,16 @@
 import java.util.logging.Logger;
-
+import  org.junit.Assert;
 public class Test {
 
     public static final Logger logger = LoggerFactory.getlogger(Test.class);
 
-    @Test
+    @org.junit.Test
     public void canarioTestCase(){
         Canario canario = new Canario();
         Assert.assertNotNull(canario);
 
         lagger.debug("canario realizaVuelo");
-        System.out.println(canario realizaVuelo);
+        System.out.println("canario realizaVuelo");
         canario.realizaVuelo();
         lagger.debug("canario realizaSonido");
         System.out.println("canario realizaSonido");
